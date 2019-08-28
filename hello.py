a934e617b41854c9b982e34ed7cbf1b4050f64a7
root <wale.adeniji@techadvance.ng>
2019-08-28 10:52:08 +0100

    First Commit Here 28082019

---
+++ hello.py
@@ -0,0 +1,7 @@
+#print ('Python is both a pretty snake and a programming language')
+x = input ("Choose a random number: ")
+x = int(x)
+if x > 12878:
+    print("greater")
+else:
+    print("smaller")
